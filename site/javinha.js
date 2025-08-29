eebcf2147f1916c1ee0bdce4037da72d6378f17c
João Wesley do Nascimento Santos <joaoweslynss@gmail.com>
2025-08-29 10:36:39 -0300

    Update javinha.js

--- site/javinha.js
+++ site/javinha.js
@@ -7,23 +7,20 @@ function Verificador() {
     const login = document.getElementById("Login").value;
     const senha = document.getElementById("Senha").value;
     const mensagem = document.getElementById("textinho");
+    let encontrado = false;
 
     if (login === "" || senha === "") {
         mensagem.innerHTML = "Verifique seu cadastro";
         return;
     }
 
-    let encontrado = false;
     for (let i = 0; i < acesso.length; i++) {
         if (login === acesso[i].login && senha === acesso[i].senha) {
-            mensagem.innerHTML = "Você esta cadastrado";
             encontrado = true;
+            mensagem.innerHTML = "Você esta cadastrado";
             document.getElementById("bemVindoOverlay").classList.add("ativo")
             mensagem.innerHTML = ""
-            window.location.href = "/site/segunda.html"
-
-
-            break;
+            window.location.href = 'http://127.0.0.1:5500/segunda.html'
         }
     }
     if (!encontrado) {
